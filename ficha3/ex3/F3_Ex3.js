123f577132bbdb384d267a8fa1215bc787ffd5c6
2190723 <2190723@my.ipleiria.pt>
2019-10-24 11:51:07 +0100

    ficha3

--- ficha3/ex3/F3_Ex3.js
+++ ficha3/ex3/F3_Ex3.js
@@ -1,5 +1,21 @@
 "use strict";
 
 function escondeMostra() {
-    
+    console.log("esconde/mostra");
+
 }
+
+function esconde() {
+    console.log("esconde");
+
+}
+
+function mostra() {
+    console.log("mostra");
+
+}
+
+document.addEventListener("DOMContentloaded", function () {
+    var botao = document.getElementById("botao");
+    botao.addEventListener("mouseover", mostra);
+});
