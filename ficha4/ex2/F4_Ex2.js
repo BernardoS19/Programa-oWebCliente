8c63754570a9287c12341c7022216885a6e600d2
2190723 <2190723@my.ipleiria.pt>
2019-10-31 12:49:51 +0000

    ficha3

--- ficha4/ex2/F4_Ex2.js
+++ ficha4/ex2/F4_Ex2.js
@@ -5,14 +5,24 @@ var imagem = null;
 function escondeMostra() {
     console.log("esconde/mostra");
 
-    var visibilidade = getComputedStyle(imagem).getPropertyValue("visibility");
+    /*var visibilidade = getComputedStyle(imagem).getPropertyValue("visibility");
 
     if (visibilidade === "visible"){
         imagem.style.visibility = "hidden";
     }else {
         imagem.style.visibility = "visible";
-    }
+    }*/
 }
+//var visibilidade = getComputedStyle(imagem).getPropertyValue("visibility");
+var visibilidade = imagem.css("visibility");
+if(visibilidade === "visible"){
+    //imagem.style.visibility = "hidden";
+    imagem.css("visibility", "hiden");
+}else{
+    //imagem.style.visibility = "visible";
+    imagem.css("visibility", "visible");
+}
+
 
 function esconde() {
     console.log("esconde");
@@ -24,8 +34,19 @@ function mostra() {
     imagem.style.display = "block";
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+/*document.addEventListener("DOMContentLoaded", function () {
     imagem = document.getElementsByTagName("img")[0];
     var botao = document.getElementById("botao");
     botao.addEventListener("mouseover", mostra);
+});*/
+
+
+//document.addEventListener("DOMContentLoaded", function () {})
+$(document).ready(function () {
+    //imagem = document.getElementsByTagName("img")[0];
+    imagem = $("img");
+
+    //var botao = document.getElementById("botao");
+    //botao.addEventListener("mouseover", mostra);
+    $("#botao").mouseover(mostra);
 });
